16ad74cf523ec8c179e3a7e5f50194b8fa4a00a1
Daniel Scheufler <djscheufler@gmail.com>
2016-10-11 13:58:28 -0500

    Initial Design Verification
    = Verified DeclareATest fluency, functionality
    = + Modified Action signature, Subject assignment to deal with Mutability issues
    = = Will consider expansion of Context mutation to Arrange later
    + Update initial implementaion demo.

--- DeclareATest/v1/DeclareATest.py
+++ DeclareATest/v1/DeclareATest.py
@@ -21,23 +21,24 @@ from .core import fluent.fluent as fluent # should import fluent method decorato
 class DeclareATest():
     def __init__(self):
         self._arrange = None
-        self._subject = None
+        self._context = {} #Need to use Context to wrapp and allow mutation of Vars
+            # Can extend this to later use. May refactor Arrange to take and mutate Context.
         self._expectations = []
-        self._action
+        self._action = None
 
     @fluent
     def _For(self,subject):
         """Takes a pre-initialized subject to test against"""
-        self._subject = subject
+        self._context["Subject"] = subject
 
     @fluent
     def ArrangedBy(self, arrangeMethod):
         """Takes a Method which returns an initialized subject, executed at Run()"""
         self._arrange = arrangeMethod
-        self._subject = None
+        self._context["Subject"] = None
 
     @fluent
-    def ThatExpected(self,expectation):
+    def ThatExpects(self,expectation):
         """Takes a method that takes subject and that returns boolean for met or unmet expecation.
             Each is evaluated at Run(). Errors are collected and reported when all expectations evaluated."""
         self._expectations.append(expectation)
@@ -55,23 +56,29 @@ class DeclareATest():
 
         try:
             #Arrange
-            if self._subject is None:
-                self._subject = self._arrange()
+            if self._context["Subject"] is None:
+                self._context["Subject"] = self._arrange()
+                print("Arranging subject")
+
+
 
             #Act
-            self._action(self._subject)
+            print("Arranged: {}".format(self._context["Subject"]))
+            self._action(self._context)
+            print("Acted: {}".format(self._context["Subject"]))
 
             #Asssert
             for expectation in self._expectations:
                 try:
-                    flag = expecation(self._subject)
-                    if !flag:
+                    flag = expectation(self._context["Subject"])
+                    print("Flag: {}".format(flag))
+                    if not flag:
                         errors.append("Expecations not met: {0}".format(expectation))
-                except:
-                    errors.append("Exception caught: {0}".format(sys.exec_info()[0]))
-        except:
-            errors.append("Exception caught: {0}".format(sys.exec_info()[0]))
+                except Exception as e:
+                    errors.append("Exception caught: {0}".format(e))
+        except Exception as e:
+            errors.append("Exception caught: {0}".format(e))
 
         if len(errors) != 0:
-            msg = '\n'.join('{},\n'.format(errors)
-            raise AssertionError("Test did not succeed. Errors {0}".format(msg))
+            #msg = '\n'.join('{},\n'.format(errors))
+            raise AssertionError("Test did not succeed. Errors {0}".format(errors))
--- DeclareATest/v1/ImplemenationExample.py
+++ DeclareATest/v1/ImplemenationExample.py
@@ -5,5 +5,18 @@ Purpose:
         series of test.
 
 Author: DJS
-Date: --
+Date: 11OCT2016
 """
+
+import DeclareATest
+
+def action(context):
+	print("	Input subject: {}".format(context["Subject"]))
+	context["Subject"]=1
+	print("	Acted subject: {}".format(context["Subject"]))
+
+subject=125
+test = DeclareATest()._For(subject).ThatExpects(lambda s: s==1).When(action)
+#test = test.ThatExpects(lambda s: s==1)
+#test = test.When(action)
+test.Run()
