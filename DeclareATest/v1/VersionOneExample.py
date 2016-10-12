87692cf9c6575a83c4b6edb269bbc912bcc14004
Daniel Scheufler <djscheufler@gmail.com>
2016-10-12 08:52:20 -0500

    Continued refinement of Example Code
    + Added Additional test cases.
    = + Added Case for Alternative Construction
    = + Added Mixed Case
    = + Added Mutli-positive case
    = + Added TC for ArrangedBy method
    
    = Need to add Context Injection for Arrange method
    = Need to Refine error messages from expectation functions.

--- DeclareATest/v1/VersionOneExample.py
+++ DeclareATest/v1/VersionOneExample.py
@@ -9,14 +9,74 @@ Date: 11OCT2016
 """
 
 import DeclareATest
+from core.TestSubject import TestSubject
 
-def action(context):
-	print("	Input subject: {}".format(context["Subject"]))
-	context["Subject"]=1
-	print("	Acted subject: {}".format(context["Subject"]))
+def InitDemo():
 
-subject=125
-test = DeclareATest()._For(subject).ThatExpects(lambda s: s==1).When(action)
-#test = test.ThatExpects(lambda s: s==1)
-#test = test.When(action)
-test.Run()
+    def action(context):
+    	print("	Input subject: {}".format(context["Subject"]))
+    	context["Subject"]=1
+    	print("	Acted subject: {}".format(context["Subject"]))
+
+    subject=125
+    test = DeclareATest()._For(subject).ThatExpects(lambda s: s==1).When(action)
+    #test = test.ThatExpects(lambda s: s==1)
+    #test = test.When(action)
+    test.Run()
+
+def TestSubjectDemo():
+
+    def action(context):
+        print(" Setting Flag to True")
+        context["Subject"].SetFlagTrue()
+
+    test = DeclareATest().ArrangedBy(lambda : TestSubject(False))\
+            .ThatExpects(lambda s: s.GetFlag()).When(action)
+    test.Run()
+    print("")
+
+    def action2(context):
+    	print("	Setting Name to John")
+    	context["Subject"].SetName("John")
+
+
+    test2 = DeclareATest().ArrangedBy(lambda : TestSubject(True))\
+    			.ThatExpects(lambda s: s.GetName() == "John")\
+    			.When(action2)
+    test2.Run()
+    print("")
+
+    def action3(context):
+    	print("	Setting Name to John")
+    	context["Subject"].SetName("John")
+    	print("	Setting Flag to True")
+    	context["Subject"].SetFlagTrue()
+
+
+    test3 = DeclareATest().ArrangedBy(lambda : TestSubject(False))\
+    			.ThatExpects(lambda s: s.GetName() == "John")\
+    			.ThatExpects(lambda s: s.GetFlag())\
+    			.When(action3)
+    test3.Run()
+    print("")
+
+    def action4(context):
+    	print("	Setting Name to James")
+    	context["Subject"].SetName("James")
+    	print("	Setting Flag to True")
+    	context["Subject"].SetFlagTrue()
+
+
+    test4 = DeclareATest().ArrangedBy(lambda : TestSubject(False))\
+    			.ThatExpects(lambda s: s.GetName() == "John")\
+    			.ThatExpects(lambda s: s.GetFlag())\
+    			.When(action4)
+    try:
+        test4.Run()
+    except AssertionError:
+        print("Test 4 has failed, as expected. Validating Name against John is incorrect.")
+
+    print("")
+
+if _name_ == "_main_":
+    TestSubjectDemo()
